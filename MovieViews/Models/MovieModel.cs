a06aef9beabc0e1ce332ea7baa03764b41924172
TaeSeok Kim <paeaned@gmail.com>
2022-06-28 16:35:38 +0900

    초기 커밋.

---
+++ MovieViews/Models/MovieModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieViews.Models
+{
+    public class MovieModel
+    {
+        public int Num { get; set; }
+        public string Name { get; set; }
+        public string ODate { get; set; }
+        public long TEarn { get; set; }
+        public double Pct { get; set; }
+        public long TAud { get; set; }
+    }
+}
---
+++ MovieViews/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,288 @@
+﻿using MovieViews.Models;
+using System;
+using System.IO;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Windows.Input;
+using System.Data;
+using System.Data.SqlClient;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace MovieViews.ViewModels
+{
+
+    public class MainWindowViewModel : Notifier
+    {
+        /// <summary>
+        /// DB연결 부분
+        /// </summary>
+        #region DbConnection
+        DataSet ds;
+        string MvJson;
+        string constr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MvvmMovieViews.Data;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        #endregion
+
+        /// <summary>
+        /// 초기 날짜 설정 (오늘의 전날)
+        /// </summary>
+        static DateTime now = DateTime.Now;
+        static string Date = now.AddDays(-1).ToString("yyyyMMdd");
+
+        static string ApiKey = "";
+
+        /// <summary>
+        /// API 연동주소 생성 메소드
+        /// </summary>
+        /// <param name="Date"></param>
+        /// <returns></returns>
+        static string RequestURL(string Date)
+        {
+            #region ApiKey 가져오기
+            string keyuri = @"..\..\ApiKey.xml";
+
+            try
+            {
+                XmlDocument xml = new();
+                xml.Load(keyuri);
+
+                XmlNodeList nodeList = xml.SelectNodes("/resources");
+                ApiKey = nodeList[0].InnerText;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            #endregion
+
+            string result = string.Format("https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key={0}&targetDt={1}", ApiKey, Date);
+            return result;
+        }
+
+        MovieModel movie = null;
+
+        /// <summary>
+        /// 메인 윈도우 시동시 작동하는 것??
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            movie = new MovieModel();
+            AddMovie();
+
+            CuDate = ChangeDate(Date);
+        }
+
+        /// <summary>
+        /// ~~~년 ~~월 ~~일 박스오피스 출력 감시 기능
+        /// </summary>
+        private string _cuDate;
+
+        public string CuDate
+        {
+            get { return _cuDate; }
+            set
+            {
+                _cuDate = value;
+                OnPropertyChanged("CuDate");
+            }
+        }
+
+        /// <summary>
+        /// 영화 콜렉션 출력 감시 기능
+        /// </summary>
+        ObservableCollection<MovieModel> movies = null;
+        public ObservableCollection<MovieModel> Movies
+        {
+            get
+            {
+                if (movies == null)
+                {
+                    movies = new ObservableCollection<MovieModel>();
+                }
+                return movies;
+            }
+            set
+            {
+                movies = value;
+                OnPropertyChanged("Movies");
+            }
+        }
+
+        /// <summary>
+        /// DB에 해당 날짜의 영화 정보가 있는지 보고 없으면 추가시켜주며 있으면 이미 있는 정보를 출력해주는 기능
+        /// </summary>
+        public void AddMovie()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(constr))
+                {
+                    conn.Open();
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM [dbo].[Movies.Logs] WHERE date = @_date", conn))
+                    {
+                        adapter.SelectCommand.Parameters.AddWithValue("@_date", Date);
+                        ds = new DataSet();
+                        adapter.Fill(ds);
+
+                        if (ds.Tables[0].Rows.Count == 0)
+                        {
+                            WebRequest request = WebRequest.Create(RequestURL(Date));
+                            request.Method = "GET";
+                            request.ContentType = "application/json";
+
+                            using (WebResponse response = request.GetResponse())
+                            using (Stream stream = response.GetResponseStream())
+                            using (StreamReader reader = new StreamReader(stream))
+                            {
+                                string data = reader.ReadToEnd();
+                                var obj = JObject.Parse(data);
+
+                                MvJson = Convert.ToString(obj);
+                                using (SqlCommand cmd = new SqlCommand("INSERT INTO [dbo].[Movies.Logs] VALUES (@_date, @_log)", conn))
+                                {
+                                    cmd.Parameters.AddWithValue("@_date", Date);
+                                    cmd.Parameters.AddWithValue("@_log", MvJson);
+                                    cmd.ExecuteNonQuery();
+                                }
+
+                                conn.Close();
+                                
+                                var boxOfficeResult = obj["boxOfficeResult"];
+                                var dailyBoxOfficeList = boxOfficeResult["dailyBoxOfficeList"];
+
+                                foreach (var item in dailyBoxOfficeList)
+                                {
+                                    long coTEarn = Convert.ToInt64(item["salesAcc"]);
+                                    double coPct = Convert.ToDouble(item["salesShare"]);
+                                    long coTAud = Convert.ToInt64(item["audiAcc"]);
+
+                                    Movies.Add(new MovieModel()
+                                    {
+                                        Num = (int)item["rank"],
+                                        Name = (string)item["movieNm"],
+                                        ODate = (string)item["openDt"],
+                                        TEarn = coTEarn,
+                                        Pct = coPct,
+                                        TAud = coTAud
+                                    });
+                                }
+                            }
+                        } else
+                        {
+                            conn.Close();
+
+                            DataTable dt = ds.Tables[0];
+
+                            var data = dt.Rows[0][2];
+                            var obj = JObject.Parse((string)data);
+
+                            var boxOfficeResult = obj["boxOfficeResult"];
+                            var dailyBoxOfficeList = boxOfficeResult["dailyBoxOfficeList"];
+
+                            foreach (var item in dailyBoxOfficeList)
+                            {
+                                long coTEarn = Convert.ToInt64(item["salesAcc"]);
+                                double coPct = Convert.ToDouble(item["salesShare"]);
+                                long coTAud = Convert.ToInt64(item["audiAcc"]);
+
+                                Movies.Add(new MovieModel()
+                                {
+                                    Num = (int)item["rank"],
+                                    Name = (string)item["movieNm"],
+                                    ODate = (string)item["openDt"],
+                                    TEarn = coTEarn,
+                                    Pct = coPct,
+                                    TAud = coTAud
+                                });
+                            }                            
+                        }
+                    }
+                }
+
+                #region 해당 날짜 영화정보 추가 (// 수정전)
+                //WebRequest request = WebRequest.Create(RequestURL(Date));
+                //request.Method = "GET";
+                //request.ContentType = "application/json";
+
+                //using (WebResponse response = request.GetResponse())
+                //using (Stream stream = response.GetResponseStream())
+                //using (StreamReader reader = new StreamReader(stream))
+                //{
+                //    string data = reader.ReadToEnd();
+                //    var obj = JObject.Parse(data);
+                //    var boxOfficeResult = obj["boxOfficeResult"];
+                //    var dailyBoxOfficeList = boxOfficeResult["dailyBoxOfficeList"];
+
+                //    foreach (var item in dailyBoxOfficeList)
+                //    {
+                //        long coTEarn = Convert.ToInt64(item["salesAcc"]);
+                //        double coPct = Convert.ToDouble(item["salesShare"]);
+                //        long coTAud = Convert.ToInt64(item["audiAcc"]);
+
+                //        Movies.Add(new MovieModel() {
+                //            Num = (int)item["rank"],
+                //            Name = (string)item["movieNm"],
+                //            ODate = (string)item["openDt"],
+                //            TEarn = coTEarn,
+                //            Pct = coPct,
+                //            TAud = coTAud
+                //        });
+                //    }
+                //}
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private ICommand preDateCommand;
+        public ICommand PreDateCommand
+        {
+            get { return (this.preDateCommand ?? (this.preDateCommand = new DelegateCommand(PreDate))); }
+        }
+        
+        private void PreDate()
+        {
+            if (Movies.Count != 0)
+            {
+                Movies.Clear();
+            }
+            DateTime tempDate = DateTime.ParseExact(Date, "yyyyMMdd", null);
+            Date = tempDate.AddDays(-1).ToString("yyyyMMdd");
+
+            CuDate = ChangeDate(Date);
+
+            AddMovie();
+        }
+
+        private ICommand nextDateCommand;
+        public ICommand NextDateCommand
+        {
+            get { return (this.nextDateCommand ?? (this.nextDateCommand = new DelegateCommand(NextDate))); }
+        }
+
+        private void NextDate()
+        {
+            if (Movies.Count != 0)
+            {
+                Movies.Clear();
+            }
+            DateTime tempDate = DateTime.ParseExact(Date, "yyyyMMdd", null);
+            Date = tempDate.AddDays(+1).ToString("yyyyMMdd");
+
+            CuDate = ChangeDate(Date);
+
+            AddMovie();
+        }
+
+        private string ChangeDate(string Date)
+        {
+            string result = DateTime.ParseExact(Date, "yyyyMMdd", null).ToString("yyyy년 MM월 dd일 박스오피스");
+            return result;
+        }
+    }   
+}
